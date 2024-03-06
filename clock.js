dc8e0a9b01dd6d5c76b457679c184c334a6d5882
CRUZMARI21 <140350193+CRUZMARI21@users.noreply.github.com>
2024-03-07 10:10:20 +1300

    Maria uploaded a clock sketch

--- clock.js
+++ clock.js
@@ -11,15 +11,12 @@ function draw_clock(obj) {
   //        < 0 if no alarm is set
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
-  background(50); //  beige
-  fill(200); // dark grey
-  textSize(40);
-  textAlign(CENTER, CENTER);
-  text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
+  
+  background('#3EC6DE'); 
+  fill(50)
 
-
-  fill(249, 140, 255);// pink
-  ellipse(width / 3, 350, 150);
+  fill('Brown')
+  ellipse(340,250,140,130);
   fill(140, 255, 251) // blue
   ellipse(width / 2, 350, 150);
   fill(175, 133, 255); // purple
